717d25020d5299949e670d1f335a9ec2330757ca
Alen Sugimoto <alensugimoto@gmail.com>
2020-12-16 00:38:28 +0100

    Add default actions to the settings screen

--- lib/model/shared_prefs.dart
+++ lib/model/shared_prefs.dart
@@ -2,4 +2,5 @@ class SharedPrefs {
   static const String GOT_STARTED = 'gotStarted';
   static const String FILE = 'file';
   static const String SNOOZE = 'snooze';
+  static const String LINK_OPTION = 'linkOption';
 }
