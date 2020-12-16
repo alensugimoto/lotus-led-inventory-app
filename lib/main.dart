eafef164f25d6b11bf152d5d4e4258ee1a18af3a
Alen Sugimoto <alensugimoto@gmail.com>
2020-12-16 15:21:02 +0100

    Integrate the flutter_downloader plugin

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,15 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 
 import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+      debug: false,
+  );
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
